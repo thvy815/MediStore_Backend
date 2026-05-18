c5449fe5bd1bee7cf880b45bf19dcf066f805d3f
KIMDUNG <23520340@gm.uit.edu.vn>
2026-05-18 11:01:44 +0700

    fix chat

--- src/main/java/com/example/medistore/config/SecurityConfig.java
+++ src/main/java/com/example/medistore/config/SecurityConfig.java
@@ -4,13 +4,15 @@ import java.util.List;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import org.springframework.security.config.Customizer;
-import org.springframework.web.cors.*;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import lombok.RequiredArgsConstructor;
 
@@ -34,6 +36,7 @@ public class SecurityConfig {
                         .requestMatchers(
                                 "/api/auth/**",
 
+                                "/ws-chat/**",
                                 // Swagger
                                 "/swagger-ui/**",
                                 "/v3/api-docs/**",
